1514bf5eb53344ac0cdf3ee59784166f9c7085d1
abhi-dot <abhiarpu2000@gmail.com>
2024-01-09 18:32:06 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import EditorComponent from "./EditorComponent"; // Adjust the path if necessary
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Demo Editor</h1>
+      <EditorComponent />
+    </div>
+  );
+}
+
+export default App;
